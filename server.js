1a24cf0e8b6e88780580e82ca44929b775220f5e
agrieby-chaniago <agrieby.chaniago@student.uhb.ac.id>
2026-04-13 15:49:42 +0700

    hallo sayang

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api/data", (req, res) => {
-  res.json({ message: "Hello from the backend!" });
+  res.json({ message: "hallo sayang" });
 });
 
 app.listen(5000, () => {
